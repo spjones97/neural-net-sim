6076efc04b99f05d965f42951235c60402334cf9
Spencer <sjones97@email.unc.edu>
2021-04-23 15:22:45 -0400

    add @types folder

--- src/setupEnzyme.ts
+++ src/setupEnzyme.ts
@@ -1,3 +1,5 @@
-import Enzyme from "enzyme";
+import * as Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-Enzyme.configure({ adapter: new Adapter() });
+
+// tslint:disable-next-line:no-any
+(Enzyme as any).configure({ adapter: new Adapter() });
